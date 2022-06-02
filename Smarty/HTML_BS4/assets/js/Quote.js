801d4c1345fbd9e2cf830af323387df89d28a57f
gdemers71 <gabrieldemers71@gmail.com>
2022-06-02 14:13:59 -0400

    finaliser la section residentiel

--- Smarty/HTML_BS4/assets/js/Quote.js
+++ Smarty/HTML_BS4/assets/js/Quote.js
@@ -1,7 +1,119 @@
 /**	QUOTE FORM
  *************************************************** **/
 //hide & show des field selon type de building
+
+function commercialFunction() {
+  $("input").keyup(function () {
+    var numberOfElevators = $("#number-of-elevators-to-be-deployed").val();
+    var totalElevators = $("#input-elevator-amount");
+    // console.log("numberOfElevator is :", numberOfElevators);
+
+    totalElevators.val(numberOfElevators);
+
+    console.log(
+      "number-of-elevators-to-be-deployed is " +
+        numberOfElevators +
+        "input-elevator-amount is " +
+        totalElevators.val()
+    );
+
+    var elevatorAmount = $("#input-elevator-amount").val();
+    var elevatorUnitPrice = $("#elevator-unit-price").val();
+    console.log("input-elevator-amount is " + elevatorAmount + "and elevatorUnitPrice is " + elevatorUnitPrice
+    );
+    var total = elevatorAmount * elevatorUnitPrice;
+
+    $("#elevator-total-price").val(Math.round(total));
+
+    if ($("#Standard").is(":checked")) {
+      $("#elevator-unit-price").val(7565);
+      var installationFees = 0.1;
+      var totalFees = installationFees * residentialTotal;
+      $("#installation-fees").val(totalFees);
+    }
+    if ($("#Premium").is(":checked")) {
+      $("#elevator-unit-price").val(12345);
+      var installationFees = 0.13;
+      var totalFees = installationFees * residentialTotal;
+      $("#installation-fees").val(totalFees);
+    }
+    if ($("#Excelium").is(":checked")) {
+      $("#elevator-unit-price").val(15400);
+      var installationFees = 0.16;
+      var totalFees = installationFees * residentialTotal;
+      $("#installation-fees").val(totalFees);
+      console.log("totalFees is " + totalFees);
+        console.log("elevator-unit-price is" + $("#elevator-unit-price").val())
+    }
+    // $("#input-elevator-amount").val();
+  });
+}
+
+function residentialFunction() {
+  var residentialElevatorUnitPrice = 0;
+  
+  $("input").change(function () {
+    
+    var apartmentsNumber = $("#number-total-of-apartments").val();
+
+    var floorsNumber = $("#number-total-of-floors").val();
+      console.log(" apartmentsNumber is " + apartmentsNumber + " floorsNumber is " + floorsNumber);
+
+    var elevatorColumnNumber = Math.ceil(floorsNumber / 20);
+      console.log("elevatorColumnNumber is " + elevatorColumnNumber)
+
+    var residentialNumberOfElevators = Math.ceil(apartmentsNumber / floorsNumber / 6);
+      console.log("residentialNumberOfElevatorsr is " + residentialNumberOfElevators)
+
+    var finalAmountOfResidentialElevators = residentialNumberOfElevators * elevatorColumnNumber;
+    $("#input-elevator-amount").val(finalAmountOfResidentialElevators);
+
+    residentialElevatorUnitPrice = $("#elevator-unit-price").val()
+      console.log("residentialElevatorUnitPrice is " + residentialElevatorUnitPrice)
+
+    var residentialTotal = finalAmountOfResidentialElevators * residentialElevatorUnitPrice;
+      console.log("residentialTotal is " + residentialTotal)
+
+    $("#elevator-total-price").val(residentialTotal);
+
+      if ($("#Standard").is(":checked")) {
+        $("#elevator-unit-price").val(7565);
+        var installationFees = 0.1;
+        var totalFees = installationFees * residentialTotal;
+        $("#installation-fees").val(totalFees);
+      }
+      if ($("#Premium").is(":checked")) {
+        $("#elevator-unit-price").val(12345);
+        var installationFees = 0.13;
+        var totalFees = installationFees * residentialTotal;
+        $("#installation-fees").val(totalFees);
+      }
+      if ($("#Excelium").is(":checked")) {
+        $("#elevator-unit-price").val(15400);
+        var installationFees = 0.16;
+        var totalFees = installationFees * residentialTotal;
+        $("#installation-fees").val(totalFees);
+        console.log("totalFees is " + totalFees);
+          console.log("elevator-unit-price is" + $("#elevator-unit-price").val())
+      }
+
+    $("#final-price").val(+totalFees + +residentialTotal)
+      
+    });
+  };
+
+
+function hybridAndCorporate(){
+  var totalNumberOfFloors = $
+  
+  var totalNumberOfoccupants = $("#Number-Of-occupants-per-floor").val()
+}
+
+
+
+
 $(function () {
+ 
   $("input[name='building-type']").click(function () {
     if ($("#residential").is(":checked")) {
       $("#number-of-apartments").show();
@@ -13,6 +125,7 @@ $(function () {
       $("#number-of-corporations").hide();
       $("#maximum-occupancy").hide();
       $("#buisness-hours").hide();
+      residentialFunction();
     }
     if ($("#commercial").is(":checked")) {
       $("#number-of-apartments").hide();
@@ -24,6 +137,7 @@ $(function () {
       $("#number-of-corporations").hide();
       $("#maximum-occupancy").hide();
       $("#buisness-hours").hide();
+      commercialFunction();
     }
     if ($("#corporate").is(":checked")) {
       $("#number-of-apartments").hide();
@@ -49,67 +163,37 @@ $(function () {
     }
   });
 
-  $("#number-of-elevators-to-be-deployed").change(function () {
-    var numberOfElevators = $("#number-of-elevators-to-be-deployed").val();
-    var totalElevators = $("#input-elevator-amount");
-    console.log("numberOfElevator is :", numberOfElevators);
-
-    totalElevators.val(numberOfElevators);
-
-    console.log(
-      "input-elevator-amount is" +
-        totalElevators +
-        "number-of-elevators-to-be-deployed is" +
-        numberOfElevators
-    );
-    $("#input-elevator-amount").val();
+  $("input[name='quality-of-elevator']").click(function () {
+    if ($("#Standard").is(":checked")) {
+      $("#elevator-unit-price").val(7565);
+      var installationFees = 0.1;
+      var totalFees = installationFees * $("#elevator-total-price").val();
+      $("#installation-fees").val(totalFees);
+    }
+    if ($("#Premium").is(":checked")) {
+      $("#elevator-unit-price").val(12345);
+      var installationFees = 0.13;
+      var totalFees = installationFees * $("#elevator-total-price").val();
+      $("#installation-fees").val(totalFees);
+    }
+    if ($("#Excelium").is(":checked")) {
+      $("#elevator-unit-price").val(15400);
+      var installationFees = 0.16;
+      var totalFees = installationFees * $("#elevator-total-price").val();
+      $("#installation-fees").val(totalFees);
+        console.log("elevator-unit-price is" + $("#elevator-unit-price").val())
+    }
   });
-});
+  
 
-$("#number-of-elevators-to-be-deployed").keypress(function () {
-  // $("#elevator-total-price").html('');
-  var elevatorAmount = $("#input-elevator-amount").val();
-  var elevatorUnitPrice = $("#elevator-unit-price").val();
-  console.log(
-    "input-elevator-amount is" +
-      elevatorAmount +
-      "and elevatorUnitPrice is" +
-      elevatorUnitPrice
-  );
-  var total = elevatorAmount * elevatorUnitPrice;
 
-  $("#elevator-total-price").val(Math.round(total));
-});
-
-// can't write letter in the fields
-$(
-  "#number-of-apartments, #number-of-floors, #number-of-basements, #number-of-companies, #number-of-parking-spots, #number-of-elevators, #number-of-corporations, #maximum-occupancy, #buisness-hours"
-).keypress(function (e) {
-  //if not numeric, then it don't let you type
-  if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-    //display error message
-    return false;
-  }
-});
-
-$("#Standard").click(function () {
-  $("#elevator-unit-price").val(7565);
-});
-$("#Premium").click(function () {
-  $("#elevator-unit-price").val(12345);
-});
-$("#Excelium").click(function () {
-  $("#elevator-unit-price").val(15400);
-});
-
-// $("#input-elevator-amount").change(function () {
-//   $("#elevator-total-price").val().change();
-// });
-
-$("#elevator-total-price").change(function () {
-  $("#installation-fees").val().change();
-});
-
-$("#installation-fees").change(function () {
-  $("final-price").val();
+  $(
+    "#number-of-apartments, #number-of-floors, #number-of-basements, #number-of-companies, #number-of-parking-spots, #number-of-elevators, #number-of-corporations, #maximum-occupancy, #buisness-hours"
+  ).keypress(function (e) {
+    //if not numeric, then it don't let you type
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      //display error message
+      return false;
+    }
+  });
 });
